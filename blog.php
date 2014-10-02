84a5d0621a313bc41fe75b8b60d765a78f3eabcd
Rahul Prajapat <rahul.prajapat9@gmail.com>
2014-10-03 03:11:59 +0530

    add to repo

---
+++ blog.php
@@ -0,0 +1,8 @@
+<?php
+require 'header.php';
+?>
+
+
+<?php
+include 'footer.php';
+?>
---
+++ footer.php
@@ -0,0 +1,28 @@
+<!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Rahul Prajapat 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
